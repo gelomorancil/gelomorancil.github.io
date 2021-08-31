89d07d5c6a97426ab0f0a5f422d1cd62a237a142
gelomorancil <54877623+gelomorancil@users.noreply.github.com>
2021-08-31 14:26:50 +0800

    aug 31

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
